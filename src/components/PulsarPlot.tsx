592b53aa458f94943ad2e6032a99bcd72010a1f6
Thom Flaherty <thom.flaherty@buildertrend.com>
2026-03-27 14:20:21 +0000

    fix: pulsar left offset to -12%

--- src/components/PulsarPlot.tsx
+++ src/components/PulsarPlot.tsx
@@ -159,7 +159,7 @@ export function PulsarPlot({ scrollProgress, isMusic }: PulsarPlotProps) {
       className="fixed pointer-events-none z-0"
       style={{
         top: "-5%",
-        left: "-11%",
+        left: "-12%",
         width: "55%",
         height: "110%",
         opacity: 0.7,
