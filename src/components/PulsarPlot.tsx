2a93785127b25a0faaf7a2846e0ed4ea5575ba95
Thom Flaherty <thom.flaherty@buildertrend.com>
2026-03-30 14:30:58 +0000

    fix: pulsar right to -14%

--- src/components/PulsarPlot.tsx
+++ src/components/PulsarPlot.tsx
@@ -171,7 +171,7 @@ export function PulsarPlot({ scrollProgress, isMusic }: PulsarPlotProps) {
       className="fixed pointer-events-none z-0"
       style={{
         top: "-5%",
-        right: "-16%",
+        right: "-14%",
         width: "55%",
         height: "110%",
         opacity: 0.7,
